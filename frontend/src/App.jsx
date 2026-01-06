4c953b5bfef22916042981c6ff5a86c560d797a7
timberlek78 <thomasboudeele1@gmail.com>
2026-01-06 17:15:51 +0100

    Fin des routes pour voir User et recette

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Recipe from './pages/Recipe.jsx';
+import Users from './pages/Users.jsx';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/recipes" element={<Recipe />} />
+			<Route path="/users" element={<Users />} />
 		</Routes>
 	);
 }
