5a5bdd97b359b3b7c2c4f0ed6295cd3382ec8ca0
timberlek78 <thomasboudeele1@gmail.com>
2026-02-02 23:54:51 +0100

    Recipe Card amélirer et grid aussi

--- frontend/src/features/recipes/RecipeCard/RecipeCard.jsx
+++ frontend/src/features/recipes/RecipeCard/RecipeCard.jsx
@@ -1,5 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import style from './recipeCard.module.css';
+import service from '../../../services/recipe.service.js';
+
+import IngredientIcon from '../../../assets/icons/recipes/ingredient.svg?react';
+import PreparationIcon from '../../../assets/icons/recipes/add/cook-time.svg?react';
+import LikeIcon from '../../../assets/icons/recipes/like.svg?react';
+import DifficultBulle from '../RecipeView/bulles/DifficultBulle.jsx';
+
+import Bulle from '../RecipeView/bulles/Bulle.jsx';
 
 function RecipeCard({ recipe }) {
 	const navigate = useNavigate();
@@ -7,6 +15,9 @@ function RecipeCard({ recipe }) {
 	const ingredients = Array.isArray(recipe.ingredients)
 		? recipe.ingredients
 		: Object.values(recipe.ingredients ?? {});
+
+	const imageUrl = service.getImage(recipe.recipe_image);
+		
 
 	const onRecipeClick = () => {
 		navigate(`/recipes/${recipe.recipe_id}`);
@@ -22,30 +33,33 @@ function RecipeCard({ recipe }) {
 			onClick={onRecipeClick}
 			style={{
 				backgroundImage: recipe.recipe_image
-					? `url(${recipe.recipe_image})`
+					? `url(${imageUrl})`
 					: 'none',
 			}}
 		>
 			<div className={style.overlay}>
-				<div className={style.title}>
-					<h2>{recipe.recipe_name}</h2>
-					<div className={style.like} onClick={onLike}>❤️</div>
+				<div className={style.haut}>
+					<button onClick={onLike} className={style.like}>
+						<LikeIcon />
+					</button>
 				</div>
-
-				<div className={style.info}>
-					<ul className={style.ing}>
-						{ingredients.map((ing) => (
-							<li key={ing.ingredient_id}>{ing.ingredient_name}</li>
-						))}
-					</ul>
+				<div className={style.bas}>
+					<div className={style.info}>
+						<div className={style.name}>{recipe.recipe_name}</div>
+						<div className={style.row} >
+							<IngredientIcon />
+							<div><b>Ingrédients</b></div>
+						</div>
+						<ul className={style.ing}>
+							{ingredients.map((ing) => (
+								<li key={ing.ingredient_id}>{ing.ingredient_name}</li>
+							))}
+						</ul>
+					</div>
 
 					<div className={style.complementaire}>
-						<div className={style.difficulte}>
-							Difficulté : {recipe.recipe_difficult}
-						</div>
-						<div className={style.preparation}>
-							{recipe.recipe_preparation_time} min
-						</div>
+						<DifficultBulle difficult={recipe.recipe_difficult} />
+						<Bulle info ={`${recipe.recipe_preparation_time} min`} icon={<PreparationIcon />} />
 					</div>
 				</div>
 			</div>
